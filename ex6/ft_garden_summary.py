657336d3d2bdbdfaccf83330621bdf55273be81d
lelero17 <leandro-emmerling@web.de>
2026-03-23 21:17:59 +0100

    fix: realign exercises to updated subject structure

--- ex6/ft_garden_summary.py
+++
@@ -1,7 +0,0 @@
-def ft_garden_summary():
-    """Display a summary of garden information."""
-    name = input("Enter garden name: ")
-    num = int(input("Enter number of plants: "))
-    print("Garden:", name)
-    print("Plants:", num)
-    print("Status: Growing well!")
